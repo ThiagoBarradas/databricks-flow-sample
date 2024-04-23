6e4fdc7d6c1168a9098bf443a5ea7a0c400d08f4
ThiagoBarradas <ThiagoBarradas>
2024-04-23 04:23:20 +0000

    fix test

--- 00-framework/tests/mocks/spark_mock.py
+++ 00-framework/tests/mocks/spark_mock.py
@@ -8,3 +8,11 @@ class SparkCatalogMock:
 class SparkMock:
     def __init__(self):
         self.catalog = SparkCatalogMock()
+        self.conf = SparkConfigMock()
+
+class SparkConfigMock:
+    def __init__(self):
+        self.configs = {}
+    
+    def set(self, key, value):
+        self.configs[key] = value    
